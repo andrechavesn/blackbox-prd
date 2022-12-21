5aa176e61e437e24dde4c93e980f73753dbb006f
wonedd <vinmarv@hotmail.com>
2022-12-21 15:21:57 -0300

    selfie is not defiened

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,9 +1,5 @@
 import React, { useContext, useEffect } from 'react';
-import OvenPlayer from 'ovenplayer';
-import { useRouter } from 'next/router';
-import { api } from '../../services/api/api';
 import { ChannelContext } from '../../contexts/ChannelContext';
-import { withSSRAuth } from '../../utils/withSSRAuth';
 
 export default function Player() {
   const { handleChannel, channel } = useContext(ChannelContext);
@@ -13,7 +9,8 @@ export default function Player() {
   // }, [router.query.id, handleChannel]);
 
   useEffect(() => {
-    if (channel) {
+    const initTerminal = async () => {
+      const OvenPlayer = await import('ovenplayer');
       const player = OvenPlayer.create('player_id', {
         autoStart: true,
         autoFallback: true,
@@ -39,8 +36,39 @@ export default function Player() {
         },
       });
       player.play();
-    }
+    };
+    initTerminal();
   }, [channel]);
+
+  // useEffect(() => {
+  //   if (channel) {
+  //     const player = OvenPlayer.create('player_id', {
+  //       autoStart: true,
+  //       autoFallback: true,
+  //       mute: false,
+  //       sources: [
+  //         {
+  //           type: channel ? channel?.value[0].name : '',
+  //           file: channel ? channel?.value[0].url : '',
+  //         },
+  //       ],
+  //       webrtcConfig: {
+  //         timeoutMaxRetry: 4,
+  //         connectionTimeout: 10000,
+  //       },
+  //       waterMark: {
+  //         text: channel ? channel.value[0].name : '',
+  //         font: {
+  //           'font-size': '20px',
+  //           color: 'yellow',
+  //           'font-weight': 'bold',
+  //         },
+  //         position: 'top-right',
+  //       },
+  //     });
+  //     player.play();
+  //   }
+  // }, [channel]);
 
   return <div id="player_id" />;
 }
