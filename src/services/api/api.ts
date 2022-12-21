4d1268754858e3b883dc09cf84d154e68b0c71f0
wonedd <vinmarv@hotmail.com>
2022-12-21 15:32:13 -0300

    https

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -2,5 +2,6 @@ import axios from 'axios';
 import { headers } from '../../utils/Headers';
 
 export const api = axios.create({
-  baseURL: 'http://web-dev.eba-jrk4uvgx.eu-west-1.elasticbeanstalk.com/api',
+  baseURL: 'https://web-dev.eba-jrk4uvgx.eu-west-1.elasticbeanstalk.com/api',
+  headers,
 });
