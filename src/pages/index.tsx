23f5ab5f26b592ede3be2739481809fc84d1cdcb
wonedd <vinmarv@hotmail.com>
2022-12-21 16:44:52 -0300

    mock

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { IoPerson, IoLockClosed } from 'react-icons/io5';
 import { useForm } from 'react-hook-form';
 
@@ -12,27 +12,28 @@ import { AuthContext } from '../contexts/AuthContext';
 export default function Login() {
   const { register, handleSubmit } = useForm();
   const { signIn, isLoading } = useContext(AuthContext);
-  const data = new FormData();
-  data.append('username', 'andre');
-  data.append('password', '123456');
+  useEffect(() => {
+    const data = new FormData();
+    data.append('username', 'andre');
+    data.append('password', '123456');
 
-  const config = {
-    method: 'post',
-    url: 'http://web-dev.eba-jrk4uvgx.eu-west-1.elasticbeanstalk.com/api/Account',
-    headers: {
-      accept: '/',
-      ...data.getHeaders(),
-    },
-    data,
-  };
+    const config = {
+      method: 'post',
+      url: 'http://web-dev.eba-jrk4uvgx.eu-west-1.elasticbeanstalk.com/api/Account',
+      headers: {
+        accept: '/',
+      },
+      data,
+    };
 
-  axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
   return (
     <Container>
       <Content onSubmit={handleSubmit(signIn)}>
